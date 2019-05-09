0eb47f055bf8f69175f09f17daca221ff433f5ab
zhenfeng <zhenfeng@wacai.com>
2019-05-09 11:57:05 +0800

    tempxx

--- mybatis-test/src/main/java/com/java/mapper/UserMapper.java
+++ mybatis-test/src/main/java/com/java/mapper/UserMapper.java
@@ -3,7 +3,11 @@ package com.java.mapper;
 import com.java.dao.User;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface UserMapper {
 
-    User getUser(@Param("id") int id);
+    User getUserById(@Param("id") int id);
+
+    List<User> getUser();
 }
--- mybatis-test/src/main/java/com/java/service/UserService.java
+++ mybatis-test/src/main/java/com/java/service/UserService.java
@@ -3,54 +3,47 @@ package com.java.service;
 import com.alibaba.fastjson.JSONObject;
 import com.java.dao.User;
 import com.java.mapper.UserMapper;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
 import java.util.List;
 
 public class UserService {
 
-    @Test
-    public void test() {
+    public SqlSessionFactory getSqlSessionFactory() {
         try {
-            String resource = "com/java/service/mybatis-config.xml";
+            String resource = "mybatis-config.xml";
             InputStream inputStream = Resources.getResourceAsStream(resource);
+            //从 XML 中构建 SqlSessionFactory
             SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-            SqlSession session = sqlSessionFactory.openSession();
-            UserMapper mapper = session.getMapper(UserMapper.class);
-            User user = mapper.getUser(1);
-            System.out.println(user.getRealName());
+            return sqlSessionFactory;
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            return null;
         }
-
-
     }
 
-    @Test
-    public void testDemo() throws IOException {
-        //mybatis配置文件的名称
-        String resource = "mybatis-config.xml";
-        //初始化mybatis配置环境
-        InputStream is = Resources.getResourceAsStream(resource);
-        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
-        //打开和数据库之间的会话
-        SqlSession session = factory.openSession();
-        //执行查询
-        List<User> ulist = session.selectList("getUser");
-        //遍历查询结果
-        for (User u : ulist) {
-            System.out.println(JSONObject.toJSONString(u));
+
+    public void test() {
+        //从 SqlSessionFactory 中获取 SqlSession
+        SqlSession session = getSqlSessionFactory().openSession();
+        try {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            User user = userMapper.getUserById(1);
+            System.out.println(user.getRealName());
+            List<User> list = userMapper.getUser();
+            for (User u : list) {
+                System.out.println(JSONObject.toJSONString(u));
+            }
+        } finally {
+            session.close();
         }
-        //关闭会话
-        session.close();
     }
+
 
 }
